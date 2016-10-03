bd3c0ae83a4fbc86ee04a3f81b1951dbaa48ca7e
Naokazu Terada <naokazu.terada@gmail.com>
2016-10-03 17:28:24 +0900

    Almost finished

--- source/script/main.js
+++ source/script/main.js
@@ -1,5 +1,27 @@
 $(function(){
 
+  $.cookie.defaults.path = '/';
+
+  // Sound
+  // --------
+
+  var sound = document.getElementById('sound');
+  sound.volume = 0.3;
+  var toggleSound = function(_on){
+    console.log(_on);
+    if(_on){
+      sound.play();
+      $.cookie('isSoundOn', 1);
+      $('#sound_btn').removeClass('off');
+    }
+    else {
+      sound.pause();
+      $.cookie('isSoundOn', 0);
+      $('#sound_btn').addClass('off');
+    }
+  }
+  toggleSound(!(isMobile.iOS() || parseInt($.cookie('isSoundOn'),10)==0));
+
   // スライダー
   // --------
   $('#slider').slick({
@@ -20,4 +42,43 @@ $(function(){
     .on('resize', function(){
       $('#slider').css('height', $(window).height()+'px');
     });
+  $('body')
+    .on('click', '#sound_btn', function(){
+      console.log('a');
+      toggleSound($(this).hasClass('off'));
+    });
 });
+
+isMobile = {
+  Android: function() {
+    if (navigator.userAgent.match(/Android/i)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  BlackBerry: function() {
+    if (navigator.userAgent.match(/BlackBerry/i)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  iOS: function() {
+    if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  Windows: function() {
+    if (navigator.userAgent.match(/IEMobile/i)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  any: function() {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows();
+  }
+};
