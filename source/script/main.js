97229db8f4612b0592915dc656757685f2653e86
Naokazu Terada <naokazu.terada@gmail.com>
2016-10-03 15:18:28 +0900

    Tweak styles

--- source/script/main.js
+++ source/script/main.js
@@ -1,1 +1,23 @@
-//= require_tree .
+$(function(){
+
+  // スライダー
+  // --------
+  $('#slider').slick({
+    dots: false,
+    draggable: false,
+    pauseOnHover: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1200,
+    prevArrow: '',
+    nextArrow: ''
+  });
+
+  $('#badge').delay(2000).fadeIn(1200);
+
+  $(window)
+    .on('resize', function(){
+      $('#slider').css('height', $(window).height()+'px');
+    });
+});
