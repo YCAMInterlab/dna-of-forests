e263f8676d17c62310f5951cc294e8e6a6f09b3d
Naokazu Terada <naokazu.terada@gmail.com>
2016-11-01 18:53:10 +0900

    Fix errors

--- source/script/components/panorama.vue
+++ source/script/components/panorama.vue
@@ -14,47 +14,48 @@ var THREE = THREELib();
 
 export default Vue.extend({
 
-  created: function() {
+  mounted: function() {
+    console.log('mounted');
 
-    var camera,
-        scene,
-        renderer,
-        raycaster,
-        mouse,
-        onPointerDownPointerX,
-        onPointerDownPointerY,
-        onPointerDownLon,
-        onPointerDownLat,
-        isUserInteracting = false,
-        onMouseDownMouseX = 0,
-        onMouseDownMouseY = 0,
-        lon = (Cookies.get('lon') || 0)*1,
-        lat = (Cookies.get('lat') || 0)*1,
-        onMouseDownLon = 0,
-        onMouseDownLat = 0,
-        phi = 0,
-        theta = 0,
-        markers = [];
+    this.camera = null;
+    this.scene = null;
+    this.renderer = null;
+    this.raycaster = null;
+    this.mouse = null;
+    this.onPointerDownLon = null;
+    this.onPointerDownLat = null;
+    // onMouseDownMouseX = 0,
+    // onMouseDownMouseY = 0,
+    // onMouseDownLon = 0,
+    // onMouseDownLat = 0,
+    this.phi = 0;
+    this.theta = 0;
+    this.lon = (Cookies.get('lon') || 0)*1;
+    this.lat = (Cookies.get('lat') || 0)*1;
+    this.onPointerDownPointerX = null;
+    this.onPointerDownPointerY = null;
+    this.isUserInteracting = false;
+    this.markers = [];
+
     // var container = document.getElementById( 'container' );
     var container = this.$el;
-    console.log('OK?', this.$el, this);
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
-    camera.target = new THREE.Vector3( 0, 0, 0 );
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
+    this.camera.target = new THREE.Vector3( 0, 0, 0 );
 
-    scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
-    raycaster = new THREE.Raycaster();
-    mouse = new THREE.Vector2();
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
 
     // 光
     var ambient = new THREE.AmbientLight(0xffffff);
-    scene.add(ambient);
+    this.scene.add(ambient);
 
     // ジオメトリの追加
 
-    var pano_sphere = this.create_pano_sphere();
-    scene.add( pano_sphere );
+    // var pano_sphere = this.create_pano_sphere();
+    // this.scene.add( pano_sphere );
 
     // グリッド
     // var sphere = new THREE.Mesh(
@@ -64,7 +65,7 @@ export default Vue.extend({
     //     wireframe: true
     //   })
     // );
-    // scene.add(sphere);
+    // this.scene.add(sphere);
 
     // 半径は全てメートルで大体目測で合わせる
     // サンプルマーカー
@@ -100,8 +101,8 @@ export default Vue.extend({
     };
     for(var key in sample_markers){
       var data = sample_markers[key];
-      var geo = translateGeoCoords(data.lat, data.lon, data.rad);
-      scene.add( create_marker('sample', geo.x, geo.y, geo.z, key) );
+      var geo = this.translateGeoCoords(data.lat, data.lon, data.rad);
+      this.scene.add( this.create_marker('sample', geo.x, geo.y, geo.z, key) );
     }
 
     // 森の知識マーカー
@@ -120,30 +121,30 @@ export default Vue.extend({
     };
     for(var key in knowledge_markers){
       var data = knowledge_markers[key];
-      var geo = translateGeoCoords(data.lat, data.lon, data.rad);
-      scene.add( create_marker('knowloedge', geo.x, geo.y, geo.z, key) );
+      var geo = this.translateGeoCoords(data.lat, data.lon, data.rad);
+      this.scene.add( this.create_marker('knowloedge', geo.x, geo.y, geo.z, key) );
     }
 
     // 座標軸の表示
     // var axis = new THREE.AxisHelper(300);
     // axis.position.set(0,-10,0); // 原点だとカメラと同じ視点になるので表示されない
-    // scene.add(axis);
+    // this.scene.add(axis);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    this.container.appendChild( renderer.domElement );
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( this.renderer.domElement );
 
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
-    document.addEventListener( 'MozMousePixelScroll', onDocumentMouseWheel, false);
+    document.addEventListener( 'mousedown', this.onDocumentMouseDown, false );
+    document.addEventListener( 'touchstart', this.onDocumentTouchStart, false );
+    document.addEventListener( 'mousemove', this.onDocumentMouseMove, false );
+    document.addEventListener( 'mouseup', this.onDocumentMouseUp, false );
+    document.addEventListener( 'mousewheel', this.onDocumentMouseWheel, false );
+    document.addEventListener( 'MozMousePixelScroll', this.onDocumentMouseWheel, false);
 
     //
 
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener( 'resize', this.onWindowResize, false );
 
     document.addEventListener( 'dragover', function( e ) {
 
@@ -183,9 +184,63 @@ export default Vue.extend({
 
     //
 
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener( 'resize', this.onWindowResize, false );
   },
+
   methods: {
+
+    animate( ts ) {
+
+      requestAnimationFrame( animate );
+      update();
+    },
+
+    render( elapsed, ts ) {
+
+      if (this.options.orbitControls) {
+        let position = this.camera.position.toArray();
+        let direction = this.target.toArray();
+        this.controls.update(position, direction);
+        this.camera.position.fromArray(position);
+        this.camera.lookAt(this.target.fromArray(direction));
+      }
+
+      if ( this.options.postprocessing ) {
+        this.postprocessing.render( elapsed, ts, this.tick );
+      } else {
+        this.renderer.render( this.scene, this.camera );
+      }
+    },
+
+    onWindowResize() {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+    },
+
+    update() {
+
+      // if ( this.isUserInteracting === false ) {
+      //   this.lon += 0.05;
+      // }
+
+      this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
+      phi = THREE.Math.degToRad( 90 - this.lat );
+      theta = THREE.Math.degToRad( this.lon );
+
+      this.camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+      this.camera.target.y = 500 * Math.cos( phi );
+      this.camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+
+      this.camera.lookAt( this.camera.target );
+
+      /*
+      // distortion
+      this.camera.position.copy( this.camera.target ).negate();
+      */
+      this.renderer.render( this.scene, this.camera );
+    },
+
     // パノラマ画像を貼り付けた球体
     create_pano_sphere(){
       var geometry = new THREE.SphereGeometry( 500, 120, 120 );
@@ -201,8 +256,136 @@ export default Vue.extend({
         map: texture
       });
       return new THREE.Mesh( geometry, material );
+    },
+
+    // 緯度経度から位置を算出
+    translateGeoCoords(latitude, longitude, radius) {
+
+      // 仰角
+      var phi = (latitude) * Math.PI / 180;
+      // 方位角
+      var theta = (longitude - 180) * Math.PI / 180;
+
+      var x = -(radius) * Math.cos(phi) * Math.cos(theta);
+      var y = (radius) * Math.sin(phi);
+      var z = (radius) * Math.cos(phi) * Math.sin(theta);
+
+      return new THREE.Vector3(x, y, z);
+    },
+
+    // マーカー
+    create_marker(type, x=0, y=0, z=0, key){
+      // 15cmくらい
+      var geometry = (type=='sample') ? new THREE.TetrahedronGeometry(0.15) : new THREE.SphereGeometry(0.15, 8, 8);
+      // geometry.scale( - 1, 1, 1 );
+      var material = new THREE.MeshLambertMaterial({
+        color: 0xffffff
+      });
+      var marker = new THREE.Mesh( geometry, material );
+
+      marker.position.set( x, y, z );
+      marker.key = key;
+      this.markers.push( marker );
+
+      return marker;
+    },
+
+    onDocumentTouchStart( e ) {
+
+      e.preventDefault();
+
+      e.clientX = e.touches[0].clientX;
+      e.clientY = e.touches[0].clientY;
+      onDocumentMouseDown( e );
+
+    },
+
+    onDocumentMouseDown( e ) {
+
+      e.preventDefault();
+
+      this.isUserInteracting = true;
+
+      this.onPointerDownPointerX = e.clientX;
+      this.onPointerDownPointerY = e.clientY;
+
+      this.onPointerDownLon = this.lon;
+      this.onPointerDownLat = this.lat;
+
+      // -----
+
+      this.mouse.x = ( e.clientX / this.renderer.domElement.clientWidth ) * 2 - 1;
+      this.mouse.y = - ( e.clientY / this.renderer.domElement.clientHeight ) * 2 + 1;
+
+      this.raycaster.setFromCamera( this.mouse, this.camera );
+
+      var intersects = this.raycaster.intersectObjects( this.markers );
+
+      if ( intersects.length > 0 ) {
+
+        var clicked_marker = intersects[0];
+
+        if(clicked_marker.object.material.color.r!=1){
+          clicked_marker.object.material.color.r = 1;
+        }
+        else{
+          clicked_marker.object.material.color.r = 0;
+        }
+
+        console.log('clicked:', clicked_marker.object.key);
+
+        // intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+        // var particle = new THREE.Sprite( particleMaterial );
+        // particle.position.copy( intersects[ 0 ].point );
+        // particle.scale.x = particle.scale.y = 16;
+        // scene.add( particle );
+
+      }
+
+      /*
+      // Parse all the faces
+      for ( var i in intersects ) {
+
+        intersects[ i ].face.material[ 0 ].color.setHex( Math.random() * 0xffffff | 0x80000000 );
+
+      }
+      */
+    },
+
+    onDocumentMouseMove( e ) {
+
+      if ( this.isUserInteracting === true ) {
+        this.lon = ( this.onPointerDownPointerX - e.clientX ) * 0.1 + this.onPointerDownLon;
+        this.lat = ( e.clientY - this.onPointerDownPointerY ) * 0.1 + this.onPointerDownLat;
+      }
+
+    },
+
+    onDocumentMouseUp( e ) {
+
+      this.isUserInteracting = false;
+
+      Cookies.set('lon', this.lon);
+      Cookies.set('lat', this.lat);
+    },
+
+    onDocumentMouseWheel( e ) {
+
+      // WebKit
+      if ( e.wheelDeltaY ) {
+        this.camera.fov -= e.wheelDeltaY * 0.05;
+      // Opera / Explorer 9
+      } else if ( e.wheelDelta ) {
+        this.camera.fov -= e.wheelDelta * 0.05;
+      // Firefox
+      } else if ( e.detail ) {
+        this.camera.fov += e.detail * 1.0;
+      }
+      this.camera.updateProjectionMatrix();
     }
+
   },
+
   data () {
     return {
     }
