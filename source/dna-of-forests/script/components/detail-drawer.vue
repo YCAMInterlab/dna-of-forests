b3c8d59c2066b23a5001cba2bc8e77b60e1b499b
Naokazu Terada <naokazu.terada@gmail.com>
2016-11-04 18:43:55 +0900

    [WIP] Show detail drawer

--- source/dna-of-forests/script/components/detail-drawer.vue
+++ source/dna-of-forests/script/components/detail-drawer.vue
@@ -6,7 +6,7 @@
       img(alt='森の知識' src='/dna-of-forests/img/detail-drawer/title-chisiki.png' srcset='/dna-of-forests/img/detail-drawer/title-chisiki@2x.png 2x')
     router-link.close_btn(to='./')
       img(alt='Close' src='/dna-of-forests/img/detail-drawer/close-btn.png' srcset='/dna-of-forests/img/detail-drawer/close-btn@2x.png 2x')
-  article
+  article {{ genus_ja }}
 
 </template>
 
@@ -39,3 +39,53 @@ header
       opacity: 0.5
 
 </style>
+
+<script>
+
+import Vue from 'vue';
+
+export default Vue.extend({
+
+  created () {
+    this.fetchData();
+  },
+
+  watch: {
+    // ルートが変更されたらfetchDataを再び呼び出す
+    '$route': 'fetchData'
+  },
+
+  data: function(){
+    return {
+      'id': null,
+      'genus_ja': null,
+      'genus_en': null,
+      'dna': null,
+      'region': null,
+      'date': null
+    }
+  },
+
+  methods: {
+    fetchData () {
+      var idx = this.$route.params.index - 1;
+      var _data = null;
+      if(0<=this.$route.path.lastIndexOf('/list/')){
+        _data = this.$root.samples[idx];
+      }
+      else{
+        _data = this.$root.knowledges[idx];
+      }
+
+      // TODO: もっとシンプルに受け渡せないか
+      this.id = _data.id;
+      this.genus_ja = _data.genus_ja;
+      this.genus_en = _data.genus_en;
+      this.dna = _data.dna;
+      this.region = _data.region;
+      this.date = _data.date;
+    }
+  }
+});
+
+</script>
--- source/dna-of-forests/script/main.js
+++ source/dna-of-forests/script/main.js
@@ -3,25 +3,18 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/',             component: require('./components/panorama.vue') },
-  { path: '/panorama',     component: require('./components/panorama.vue') },
-  { path: '/panorama/:id', component: require('./components/panorama.vue') },
-  { path: '/list',         component: require('./components/list.vue') },
-  { path: '/list/:id',     component: require('./components/list.vue') },
-  { path: '/about',        component: require('./components/about.vue') },
-]
-
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/',                component: require('./components/panorama.vue') },
+    { path: '/panorama',        component: require('./components/panorama.vue') },
+    { path: '/panorama/:index', component: require('./components/panorama.vue') },
+    { path: '/list',            component: require('./components/list.vue') },
+    { path: '/list/:index',     component: require('./components/list.vue') },
+    { path: '/about',           component: require('./components/about.vue') },
+  ]
 })
 
 // 登録
@@ -31,5 +24,8 @@ Vue.component('detail-drawer', require('./components/detail-drawer.vue'));
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
 const app = new Vue({
-  router
+  router: router,
+  data: function(){
+    return require('./data.json');
+  }
 }).$mount('#app')
