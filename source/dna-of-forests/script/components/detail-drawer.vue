f171a86877585b97fb636492f3a075a87bbe64cd
Naokazu Terada <naokazu.terada@gmail.com>
2016-11-04 15:52:12 +0900

    detail-drawerのヘッダ部分のスタイル

--- source/dna-of-forests/script/components/detail-drawer.vue
+++ source/dna-of-forests/script/components/detail-drawer.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
 
 .drawer
+  header
+    h2
+      img(alt='森の知識' src='/dna-of-forests/img/detail-drawer/title-chisiki.png' srcset='/dna-of-forests/img/detail-drawer/title-chisiki@2x.png 2x')
+    router-link.close_btn(to='../')
+      img(alt='Close' src='/dna-of-forests/img/detail-drawer/close-btn.png' srcset='/dna-of-forests/img/detail-drawer/close-btn@2x.png 2x')
 
 </template>
 
@@ -13,5 +18,22 @@
   right: 0
   background-color: #1a1a1a
   z-index: 9999
+
+header
+  background-color: #2b2b2b
+  padding: 28px 25px
+  position: relative
+  h2
+    text-align: center
+  .close_btn
+    display: inline-block
+    padding: 28px 25px
+    position: absolute
+    top: 0
+    right: 0
+    cursor: pointer
+    opacity: 0.15
+    &:hover
+      opacity: 0.5
 
 </style>
