bf3ac32de6bb2631231978736e115e791e5bd56c
Naokazu Terada <naokazu.terada@gmail.com>
2016-11-06 16:39:08 +0900

    Reshape data structure

--- source/dna-of-forests/script/components/list.vue
+++ source/dna-of-forests/script/components/list.vue
@@ -9,18 +9,18 @@ section
         th.index No.
         th.genus_ja 属名
         th.genus_en 属名（英）
-        th.dna DNA配列
-        th.region 同定に用いたDNA領域
-        th.date 採取日
+        th.dna_region 同定に用いたDNA領域
+        th.dna_text DNA配列
+        th.collection_date 採取日
     tbody
       tr(v-for="(item, index) in samples" v-on:click="$router.push('/list/'+(index+1))")
         td.index {{ (index+1) | zero-pad }}
         td.genus_ja {{ item.genus_ja }}
         td.genus_en {{ item.genus_en }}
-        td.dna
-          <dna-barcode :dna="item.dna">
-        td.region {{ item.region }}
-        td.date {{ item.date }}
+        td.dna_region {{ item.dna_sequences[0].region }}
+        td.dna_text
+          <dna-barcode :dna="item.dna_sequences[0].text">
+        td.collection_date {{ item.collection_date }}
   h1
     img(alt='森の知識' src='img/list/title-knowledges.png' srcset='img/list/title-knowledges@2x.png 2x')
   table#knowledge
