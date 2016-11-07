f8ca691c5cd33ca33bea35d8ffd64fa5e434d8cd
Naokazu Terada <naokazu.terada@gmail.com>
2016-11-07 22:54:32 +0900

    Add component dna-tab

--- source/dna-of-forests/script/components/dna-barcode.vue
+++ source/dna-of-forests/script/components/dna-barcode.vue
@@ -13,37 +13,58 @@ export default Vue.extend({
   props: ['dna'],
 
   mounted: function() {
+    this.renderBarcode()
+  },
 
-    var x = 0,
-        y = 0,
-        w = 2,
-        h = 10,
-        canvas = this.$el;
+  watch: {
+    dna: function (newVal) {
+      this.renderBarcode()
+    }
+  },
 
-    canvas.width = w*this.dna.length;
-    canvas.height = h;
+  methods: {
+    renderBarcode(){
+      var x = 0,
+          y = 0,
+          w = 2,
+          h = 10,
+          margin = 10,
+          canvas = this.$el;
 
-    if (canvas.getContext) {
+      var total_w = w*this.dna.length;
 
-      var context = canvas.getContext('2d');
+      // if(this.orikaeshi){
+      //   var lines = (total_w / this.orikaeshi) + (total_w % this.orikaeshi)*1;
+      //   canvas.height = h*lines + margin*(lines-1);
+      //   canvas.width = this.orikaeshi;
+      // }
+      // else{
+        canvas.height = h;
+        canvas.width = total_w;
+      // }
 
-      var colors = {
-        'A': "rgb(255,  51, 165)", // R: #ff33a5
-        'T': "rgb( 51, 255, 109)", // G: #33ff6d
-        'G': "rgb( 51, 126, 255)", // B: #337eff
-        'C': "rgb(245, 255, 138)", // Y: #f5ff8a
-      }
+      if (canvas.getContext) {
 
-      for(var i = 0; i < this.dna.length; i++){
-        var c = this.dna[i];
-        if(colors[c]!==null){
-          context.fillStyle = colors[c];
+        var context = canvas.getContext('2d');
+
+        var colors = {
+          'A': "rgb(255,  51, 165)", // R: #ff33a5
+          'T': "rgb( 51, 255, 109)", // G: #33ff6d
+          'G': "rgb( 51, 126, 255)", // B: #337eff
+          'C': "rgb(245, 255, 138)", // Y: #f5ff8a
         }
-        else{
-          context.fillStyle = "rgb(0, 0, 0)";
+
+        for(var i = 0; i < this.dna.length; i++){
+          var c = this.dna[i];
+          if(colors[c]!==null){
+            context.fillStyle = colors[c];
+          }
+          else{
+            context.fillStyle = "rgb(0, 0, 0)";
+          }
+          x = (w*i);
+          context.fillRect(x,y,w,h);
         }
-        x = (w*i);
-        context.fillRect(x,y,w,h);
       }
     }
   }
