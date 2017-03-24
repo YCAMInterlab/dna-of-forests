113ff3793b07795217e250a0f597845d8d433a5c
Naokazu Terada <naokazu.terada@gmail.com>
2017-03-24 12:50:12 +0900

    Fix: [Vue warn]: Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders.

--- source/dna-of-forests/components/imgr.vue
+++ source/dna-of-forests/components/imgr.vue
@@ -13,15 +13,15 @@ export default Vue.extend({
   mounted: function() {
     this.$el.alt = this.alt;
     // 相対パスを絶対パスに
-    this.src = '/dna-of-forests/img/'+this.src;
+    var _src = '/dna-of-forests/img/'+this.src;
     if(this.locale){
       // locale属性があれば、言語毎に画像パスを切り替え
-      this.$el.src = this.src.replace(/(\.\w+)$/, '-'+this.$root.$i18n.locale+'$1');
-      this.$el.srcset = this.src.replace(/(\.\w+)$/, '-'+this.$root.$i18n.locale+'@2x$1')+' 2x';
+      this.$el.src = _src.replace(/(\.\w+)$/, '-'+this.$root.$i18n.locale+'$1');
+      this.$el.srcset = _src.replace(/(\.\w+)$/, '-'+this.$root.$i18n.locale+'@2x$1')+' 2x';
     }
     else{
-      this.$el.src = this.src;
-      this.$el.srcset = this.src.replace(/(\.\w+)$/, '@2x$1')+' 2x';
+      this.$el.src = _src;
+      this.$el.srcset = _src.replace(/(\.\w+)$/, '@2x$1')+' 2x';
     }
   }
 });
