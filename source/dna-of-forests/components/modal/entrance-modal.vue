e4424bd2361505278be4b3b8b208ac2c9c9d721a
Naokazu Terada <naokazu.terada@gmail.com>
2017-10-23 23:18:07 +0900

    Fix entrance link

--- source/dna-of-forests/components/modal/entrance-modal.vue
+++ source/dna-of-forests/components/modal/entrance-modal.vue
@@ -5,7 +5,7 @@
     .modal
       imgr.logo(:alt="$t('top.logo')" src='entrance-modal/logo.png' locale)
       imgr.lead(:alt="$t('top.lead')" src='entrance-modal/lead.png' locale)
-      router-link(to='./panorama') ENTER
+      router-link(v-bind:to="'/'+$route.params.forest+'/panorama'") ENTER
 
 </template>
 
